279d804efb6206c69c3a589efee6a8a55d50be20
Mahdi Karimi <kmahdi1997@gmail.com>
2021-11-11 19:38:25 +0330

    edit

--- accordion/myScript.js
+++ accordion/myScript.js
@@ -1,5 +1,5 @@
 document.write("hello")
-const acc = document.getElementsByClassName("box");
+var acc = document.getElementsByClassName("box");
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener('click', function(){
         this.classList.toggle('active')
